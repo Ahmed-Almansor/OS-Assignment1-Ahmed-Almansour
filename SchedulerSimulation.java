513880355d1ac82b9034648587ad9cf6e6201d8c
Ahmed Almansour <445050275@std.psau.edu.sa>
2026-04-16 15:08:19 +0300

    Feature 1: Added priority field to Process class and displayed it when added to ready queue

--- SchedulerSimulation.java
+++ SchedulerSimulation.java
@@ -29,6 +29,7 @@ class Process implements Runnable {
     private int burstTime; // Total time the process requires to complete (in milliseconds)
     private int timeQuantum; // Time slice (time quantum) allowed per CPU access (in milliseconds)
     private int remainingTime; // Time left for the process to finish its execution
+    private int priority;// Feature 1: priority field for each process (1-5, 5 is highest)
 
     // Constructor to initialize the process with name, burst time, and time quantum
     public Process(String name, int burstTime, int timeQuantum) {
@@ -36,6 +37,7 @@ class Process implements Runnable {
         this.burstTime = burstTime;
         this.timeQuantum = timeQuantum;
         this.remainingTime = burstTime; // Initially, remaining time is equal to the burst time
+        this.priority = 1 + new Random().nextInt(5);// Feature 1: generate random priority for each process
     }
 
     // This method will be called when the thread for this process is started
@@ -137,6 +139,10 @@ class Process implements Runnable {
         return remainingTime;
     }
 
+    // Feature 1: getter to access process priority
+    public int getPriority() {
+        return priority;
+    }
     // Check if the process has finished (i.e., no remaining time)
     public boolean isFinished() {
         return remainingTime <= 0;
@@ -294,6 +300,6 @@ public class SchedulerSimulation {
         System.out.println(Colors.BLUE + "  ➕ " + Colors.BOLD + Colors.CYAN + process.getName() + 
                           Colors.RESET + Colors.BLUE + " added to ready queue" + Colors.RESET + 
                           " │ Burst time: " + Colors.YELLOW + process.getBurstTime() + "ms" + 
-                          Colors.RESET);
+                          Colors.RESET + " │ Priority: " + Colors.YELLOW + process.getPriority() + Colors.RESET);
     }
 }
